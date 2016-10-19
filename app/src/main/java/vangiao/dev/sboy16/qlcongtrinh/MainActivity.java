6b030e2b6e8f2f89e10e598616b935387138bc69
SBOY16\Sboy16 <loveme05ye@gmail.com>
2016-10-19 08:45:27 +0700

    Initial commit

---
+++ app/src/main/java/vangiao/dev/sboy16/qlcongtrinh/MainActivity.java
@@ -0,0 +1,15 @@
+package vangiao.dev.sboy16.qlcongtrinh;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity
+{
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
